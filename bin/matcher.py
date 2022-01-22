0e316d7697915c20e6d42934461cfc5420fc4d9e
Nourdine Bah <nourdine.bah@crick.ac.uk>
2022-01-22 09:57:03 +0000

    Add comments in bin/matcher.py

--- bin/matcher.py
+++ bin/matcher.py
@@ -6,21 +6,34 @@ import numpy as np
 from scipy.stats import entropy
 import pandas as pd
 
-hamming_path = sys.argv[1]
-reads_path = sys.argv[2]
-coords_path = sys.argv[3]
-base_path = sys.argv[4]
-max_distance = int(sys.argv[5])
-max_matches = int(sys.argv[6])
-max_entropy = float(sys.argv[7])
+#hamming_path = sys.argv[1]
+#reads_path = sys.argv[2]
+#coords_path = sys.argv[3]
+#base_path = sys.argv[4]
+#max_distance = int(sys.argv[5])
+#max_matches = int(sys.argv[6])
+#max_entropy = float(sys.argv[7])
 
-#hamming_path = "results/files/integration/imran_puck_15_humangbm.ordered.hamming.csv"
-#reads_path = "results/files/integration/imran_puck_15_humangbm.reads_per_barcode.csv"
-#coords_path = "pucks/puck14.csv"
-#base_path = "tmp/matching"
-#max_distance = 4
-#max_matches = 5
-#max_entropy = .5
+hamming_path = "results/qc/210611_imran_puck_15_humangbm.ordered.hamming.csv"
+reads_path = "results/qc/210611_imran_puck_15_humangbm.reads_umis_per_barcode.csv"
+coords_path = "pucks/210611_imran_puck_15_humangbm.csv"
+base_path = "tmp/matching"
+max_distance = 4
+max_matches = 5
+max_entropy = .5
+
+"""
+The hamming distance data frame contains 4 colummns:
+ * **SeqBarcode**: the illumina barcode
+ * **Distance**: the smallest hamming distance that can be computed between an illumin barcode and one or several puck barcode
+ * **Number**: the number of puck barcodes whose the hamming distance from the illumina barcode is the smallest that can be found
+ * **PuckBarcode**: the list of puck barcodes (up to 10 items) whose the hamming distance from the illumina barcode is the smallest that can be found
+
+The procedure includes 3 successive steps and is the following:
+ 1. If the distance is 0 then there is only one match, so we include it;
+ 2. If the distance is greater than 0 but there is only one match then we include it no matter what the distance is;
+ 3. If the match doesn't fall into the situations 1 or 2, then if the distance is lesser than or equal to {max_distance} and the number of matches is lesser than or equal to {max_matches}, then we take the match if the entropy of the reads in less than or equal than {max_entropy}.
+"""
 
 # minimum hamming distance for each, number of reads per barcodes and
 # coordinates
@@ -48,7 +61,6 @@ df = df\
 		Number=lambda x: np.where(x.Number.isna(), 0, x.Number),
 		Reads=lambda x: np.where(x.Reads.isna(), 0, x.Reads)
 	)
-
 
 # working data frame
 ddf = df.set_index("Sequenced").xs("SEQUENCED").reset_index(drop=True)
