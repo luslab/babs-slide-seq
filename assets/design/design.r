280423b65f7cb038b33f0159da0f2fb228a12bc3
Nourdine Bah <nourdine.bah@crick.ac.uk>
2022-02-18 15:02:29 +0000

    Add more recent config

--- assets/design/design.r
+++ assets/design/design.r
@@ -9,14 +9,16 @@ struct <-
 	readr::read_csv("read_structures.csv") %>%
 	dplyr::mutate(run=as.character(run))
 
+puck <- readr::read_csv("pucks.csv")
 
 df <-
-	readr::read_csv("samples.csv") %>%
+	readr::read_csv("PM21231_design.csv") %>%
 	dplyr::select(sample_name, species, fastq_1, fastq_2) %>%
 	dplyr::distinct() %>%
 	dplyr::rename("name"=sample_name) %>%
-	dplyr::mutate(puck=name) %>%
-	dplyr::mutate(run=gsub("_.*", "", name)) %>%
+	dplyr::mutate(name=gsub("_21.*", "", name)) %>%
+	dplyr::left_join(puck, by="name") %>%
+	dplyr::mutate(run=gsub("_.*", "", puck)) %>%
 	dplyr::left_join(struct, by="run") %>%
 	dplyr::left_join(annot, by="species")
 
